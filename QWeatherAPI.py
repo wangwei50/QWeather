5a9902f0a744f2768a64a65e56e99803e426c456
kenking_wang <1726187@qq.com>
2024-12-17 18:01:30 +0800

    update

--- QWeatherAPI.py
+++ QWeatherAPI.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# @author: InTereSTingHE
+# @author: wangwei50
 
 import requests
 import json
@@ -16,7 +16,7 @@ url_api_geo = 'https://geoapi.qweather.com/v2/city/'
 url_api_rain = 'https://devapi.qweather.com/v7/minutely/5m'
 url_api_air = 'https://devapi.qweather.com/v7/air/now'
 
-def get(api_type):
+def get(city_id, api_type):
     url = url_api_weather + api_type + '?location=' + city_id + mykey
     return requests.get(url).json()
 
@@ -42,29 +42,14 @@ def get_city(city_kw):
 
     return city_id, district_name, city_name, province_name, country_name, lat, lon
 
-def now():
-    return get_now['now']
-
-def daily():
-    return get_daily['daily']
-
-def hourly():
-    return get_hourly['hourly']
-
-if __name__ == '__main__':
-    if KEY == '':
-        print('No Key! Get it first!')
-
-    print('请输入城市:')
-    city_input = input()
+def getWeatherByCity(city_input):
     city_idname = get_city(city_input)
-
     city_id = city_idname[0]
 
-    get_now = get('now')
-    get_daily = get('3d') # 3d/7d/10d/15d
-    get_hourly = get('24h') # 24h/72h/168h
-    get_rain = rain(city_idname[5], city_idname[6]) # input longitude & latitude
+    get_now = get(city_id, 'now')
+    get_daily = get(city_id, '3d') # 3d/7d/10d/15d
+    get_hourly = get(city_id, '24h') # 24h/72h/168h
+    #get_rain = rain(city_idname[5], city_idname[6]) # input longitude & latitude
     air_now = air(city_id)['now']
 
     #print(json.dumps(get_now, sort_keys=True, indent=4))
@@ -74,11 +59,20 @@ if __name__ == '__main__':
         print(city_idname[3], str(city_idname[2]) + '市', str(city_idname[1]) + '区')
     print('当前天气：', get_now['now']['text'], get_now['now']['temp'], '°C', '体感温度', get_now['now']['feelsLike'], '°C')
     print('空气质量指数：', air_now['aqi'])
-    print('降水情况：', get_rain)
-    print('今日天气：', daily()[0]['textDay'], daily()[0]['tempMin'], '-', daily()[0]['tempMax'], '°C')
+    #print('降水情况：', get_rain)
+    print('今日天气：', get_daily['daily'][0]['textDay'], get_daily['daily'][0]['tempMin'], '-', get_daily['daily'][0]['tempMax'], '°C')
 
     nHoursLater = 1 # future weather hourly
-    print(nHoursLater, '小时后天气：', hourly()[1]['text'], hourly()[1]['temp'], '°C')
+    print(nHoursLater, '小时后天气：', get_hourly['hourly'][1]['text'], get_hourly['hourly'][1]['temp'], '°C')
 
     nDaysLater = 1 # future weather daily
-    print(nDaysLater, '天后天气：', daily()[nDaysLater]['textDay'], daily()[nDaysLater]['tempMin'], '-', daily()[nDaysLater]['tempMax'], '°C')
+    print('明天天气：', get_daily['daily'][nDaysLater]['textDay'], get_daily['daily'][nDaysLater]['tempMin'], '-', get_daily['daily'][nDaysLater]['tempMax'], '°C')
+    return
+
+if __name__ == '__main__':
+    if KEY == '':
+        print('No Key! Get it first!')
+
+    getWeatherByCity('上海')
+    getWeatherByCity('诸暨市')
+    
